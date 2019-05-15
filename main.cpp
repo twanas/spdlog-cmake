64250bb2008c6a1f7eb37b6ea8c638b4e5fd21b2
twanas <antoine@reltech.com>
2019-05-15 21:08:50 +1000

    Added spdlog examples

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
-#include <iostream>
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/basic_file_sink.h"
+
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+
+  spdlog::info("Sample Info output.");
+  spdlog::warn("Sample Warn output.");
+  spdlog::error("Sample Error output.");
+
+  auto filelog = spdlog::basic_logger_mt("sample-logger", "sample-log.txt");
+
+  filelog.get()->info("Sample Info output.");
+  filelog.get()->warn("Sample Warn output.");
+  filelog.get()->error("Sample Error output.");
+
   return 0;
 }
